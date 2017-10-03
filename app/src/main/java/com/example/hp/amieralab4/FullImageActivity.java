360126297604121df65c5753e103783f4332f8d3
amierarosly <mierarosly@gmail.com>
2017-10-03 22:01:15 +0800

    Initial commit

---
+++ app/src/main/java/com/example/hp/amieralab4/FullImageActivity.java
@@ -0,0 +1,34 @@
+package com.example.hp.amieralab4;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+/**
+ * Created by student8 on 10/2/17.
+ */
+
+public class FullImageActivity extends Activity {
+    @Override
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.full_image);
+
+        //get intent data
+        Intent i = getIntent();
+
+        //selected image id
+        int position = i.getExtras().getInt("id");
+        ImageAdapter imageAdapter = new ImageAdapter(this);
+
+        ImageView imageView = (ImageView) findViewById(R.id.full_image_view);
+        imageView.setImageResource(imageAdapter.mThumbIds[position]);
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/hp/amieralab4/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.hp.amieralab4;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gridView = (GridView) findViewById(R.id.gridView);
+
+        //instance of ImageAdapter(this));
+        gridView.setAdapter(new ImageAdapter(this));
+
+        /**
+         * On Click event for Single GridView Item
+         */
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        @Override
+        public void onItemClick(AdapterView<?> parent, View v, int position, long id){
+
+            //sending image id to FullScreenActivity
+            Intent i = new Intent(getApplicationContext(), FullImageActivity.class);
+
+            //passing array index
+            i.putExtra("id",position);
+            startActivity(i);
+        }
+
+
+
+
+    }
+
+
+
+
+
+        );
+    }
+}
